d7dd1749f6c9d2549e7f7ed41f775991eea33ce5
Boris Evtimov <borisevtimov1980@gmail.com>
2025-12-13 22:08:21 +0200

    Translate comments from Bulgarian to English

--- smart_barrier_version_5.1.py
+++ smart_barrier_version_5.1.py
@@ -8,23 +8,22 @@ import numpy as np
 
 print("CUDA available:", torch.cuda.is_available())
 
-# База данни с валидни номера
+# Database of valid numbers
 VALID_IDS = {"CA1234BG", "CB5678SO", "TX9876NY", "CA5069MA", "CA5969MA"}
 
-# Свързване към Micro:bit (COM5)
+# Connecting to Micro:bit (COM5) in this case you need to change it to the one your Micro:bit is connected to
 microbit = serial.Serial("COM5", 115200)
 
-# OCR engine с GPU
+# OCR engine with GPU
 reader = easyocr.Reader(['en'], gpu=True)
 
 
-# Корекция на OCR грешки
+# OCR Error Correction
 def correct_plate(text):
     corrections = {'O': '0', 'Q': '0', 'I': '1', 'L': '1', 'S': '5', 'B': '8', 'Z': '2', 'G': '6'}
     return ''.join(corrections.get(c, c) for c in text.upper())
 
-
-# Проверка на формата: 2 букви + 4 цифри + 2 букви
+# Format check: 2 letters + 4 digits + 2 letters (you can customize it according to your situation or as it is in your country)
 def is_valid_format(text):
     return re.match(r'^[A-Z]{2}[0-9]{4}[A-Z]{2}$', text) is not None
 
@@ -42,19 +41,19 @@ while True:
 
     results = reader.readtext(frame, allowlist='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ')
 
-    # комбинираме всички OCR резултати в една линия
+    # combine all OCR results into a single line
     combined_text = "".join([t for (_, t, _) in results])
     combined_text = combined_text.replace(" ", "")
     corrected = correct_plate(combined_text)
 
-    # рисуваме рамки за всеки OCR блок
+   # draw frames for each OCR block
     for bbox, text, conf in results:
         pts = np.array(bbox, dtype=np.int32).reshape((-1, 1, 2))
         cv2.polylines(frame, [pts], isClosed=True, color=(255, 255, 0), thickness=2)
         cv2.putText(frame, text, tuple(pts[0][0]),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 0), 2)
 
-    # проверка на комбинирания текст
+    # check of the combined text
     if is_valid_format(corrected):
         scan_count += 1
         plates_buffer.append(corrected)
@@ -68,7 +67,7 @@ while True:
             found_valid = True
             time.sleep(2)
 
-    # ако стигнем 10 номера без валиден → INVALID
+    # if we reach 10 numbers without a valid one → INVALID
     if scan_count >= 10 and not found_valid:
         print("INVALID (след 10 номера)")
         microbit.write(b"INVALID\n")
@@ -76,7 +75,7 @@ while True:
         scan_count = 0
         time.sleep(2)
 
-    # показваме брояча върху видеото
+   # show the counter on the video
     cv2.putText(frame, f"Сканирани: {scan_count}/10", (30, 40),
                 cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 255), 2)
 
